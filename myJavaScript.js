b0eb90cf1fbc5bee6ca957a8987c1f6c130afcc6
Fiona Soetrisno <fiona.soetrisno@gmail.com>
2017-07-31 07:57:00 -0700

    Version 3 of Website, fixed home page

---
+++ myJavaScript.js
@@ -0,0 +1,14 @@
+var images = ["Pictures/lookingout.jpg", "Pictures/puddle.jpg", "Pictures/smile.jpg"];
+var i = -1;
+
+/*window.onload = function imageChange() {
+  setInterval(
+  function() {
+    var newBackground = "url(" + images[i + 1] + ")";
+    document.getElementById("background").style.backgroundImage = newBackground;
+    i = i + 1;
+    if (i == 2) {
+      i = -1;
+    }
+  }, 10000);
+}
