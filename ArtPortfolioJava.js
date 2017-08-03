0c0427f5caa4e3cef02fda1132b2e892e917e928
Fiona Soetrisno <fiona.soetrisno@gmail.com>
2017-08-03 08:05:32 -0700

    finished art portfolio

--- ArtPortfolioJava.js
+++ ArtPortfolioJava.js
@@ -1,8 +1,30 @@
-var images = ["Pictures/ArtPortfolio/swordgirl.jpg", "Pictures/ArtPortfolio/spring.jpg", "Pictures/ArtPortfolio/moon.png"];
+var images =
+["Pictures/ArtPortfolio/swordgirl.jpg",
+"Pictures/ArtPortfolio/moon.png",
+"Pictures/ArtPortfolio/theirEyes1.jpg",
+"Pictures/ArtPortfolio/theirEyes2.jpg",
+"Pictures/ArtPortfolio/charasheet_alba.jpg",
+"Pictures/ArtPortfolio/Alba_SpriteSheet.png",
+"Pictures/ArtPortfolio/charasheet_player.jpg",
+"Pictures/ArtPortfolio/Player_SpriteSheet.png",
+"Pictures/ArtPortfolio/gallade.jpg",
+"Pictures/ArtPortfolio/meganium.jpg",
+"Pictures/ArtPortfolio/scenery.jpg",
+"Pictures/ArtPortfolio/nina.jpg",
+"Pictures/ArtPortfolio/2girls.jpg",
+"Pictures/ArtPortfolio/hikari.jpg",
+"Pictures/ArtPortfolio/spring.jpg",
+"Pictures/ArtPortfolio/BlaWhi.jpg"
+];
 var i = -1;
+var numImages = images.length;
 
+window.onload = function imageStart() {
+  document.getElementById("imagePortfolio").src = images[i + 1];
+  i = i + 1;
+}
 function photoRight() {
-  if (i == 2 || i == 3) {
+  if (i == numImages - 1 || i == numImages) {
     i = -1;
   }
   document.getElementById("imagePortfolio").src = images[i + 1];
@@ -13,7 +35,7 @@ function photoRight() {
 function photoLeft() {
 
   if (i == 0 || i == -1) {
-    i = 3;
+    i = numImages;
   }
   document.getElementById("imagePortfolio").src = images[i - 1];
   i = i - 1;
